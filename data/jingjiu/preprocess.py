81a921c1cf68a3bf7e6fe5ae8f8080bfb42d270e
zhaoqiman <zhaoqiman@foxmail.com>
2024-10-27 19:19:38 +0800

    Add dataset related files.

---
+++ data/jingjiu/preprocess.py
@@ -0,0 +1,22 @@
+
+
+import json
+
+jsonPath = r"ChatLM\nanoGPT\data\jingjiu\tianji-etiquette-chinese-v0.1.json"
+
+# Load the JSON data from a file
+with open(jsonPath, 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+# Extract the "output" values
+outputs = []
+for item in data:
+    for conversation in item['conversation']:
+        outputs.append(conversation['output'])
+
+# Write the outputs to a text file
+with open('./jingjiu.txt', 'w', encoding='utf-8') as output_file:
+    for output in outputs:
+        output_file.write(output + '\n')
+
+print("Outputs have been extracted to jingjiu.txt.")
